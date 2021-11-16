44621b4f8877277e2ca68ff5b26977125a1502bc
lilibyte <lily@lilibyte.net>
2021-11-16 11:20:51 -0600

    use fileinput instead of stdin for argv support

--- misc/tac.py
+++ misc/tac.py
@@ -1,4 +1,4 @@
 # qorg BTFO once again
 # <~qorg> lilibyte: want a programming challenge?
 #         rewrite tac(1)
-print(*[line.strip() for line in __import__('sys').stdin.readlines()[::-1]], sep="\n")
+print(*[line.strip() for line in __import__('fileinput').input()][::-1], sep="\n")
