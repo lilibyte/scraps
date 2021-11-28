83cd3bc846a42fbe1a463581cc1f6aafe401d08b
lilibyte <lily@lilibyte.net>
2021-11-28 11:29:05 -0600

    pyunixmini.py: remove 10 bytes

--- misc/pyunixsock/pyunixmini.py
+++ misc/pyunixsock/pyunixmini.py
@@ -1,1 +1,1 @@
-(lib := __import__("ctypes").CDLL("rakunix.so"), (s := lib.create_socket(), lib.connect_socket(s, b"socket"), lib.write_to_sock(s, b'hello', 5), lib.close_socket(s)))
+(l := __import__("ctypes").CDLL("rakunix.so"), (s := l.create_socket(), l.connect_socket(s, b"socket"), l.write_to_sock(s, b'hello', 5), l.close_socket(s)))
